ca0e719fd27e82cdb141d4c2515d5004a9aa059b
john <johnzzu@163.com>
2019-09-16 08:50:09 +0800

    [ADD]day2-web.py

---
+++ www/app.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python/env
+
+from aiohttp import web
+
+
+async def index(request):
+    return web.Response(body=b'<h1>Awesome</h1>',content_type='text/html')
+
+
+app = web.Application()
+app.add_routes([web.get('/', index)])
+web.run_app(app, host='127.0.0.1', port=8090)
